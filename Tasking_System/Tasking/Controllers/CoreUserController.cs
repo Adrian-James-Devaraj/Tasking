19f118b2fbe9b3953ee559a008bde338af8da005
Adrian Devaraj <adrian.devaraj@ix.co.za>
2018-07-30 16:50:14 +0200

    Added Taking Function And Sustom Login

---
+++ Tasking_System/Tasking/Controllers/CoreUserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Tasking.Models;
+
+namespace Tasking.Controllers
+{
+    public class CoreUserController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: CoreUser
+        public ActionResult Index()
+        {
+            return View(db.CoreUsers.ToList());
+        }
+
+        public ActionResult MyAccount()
+        {
+            return View(db.CoreUsers.SingleOrDefault(x => x.UserName.Equals(User.Identity.Name)));
+        }
+    }
+}
---
+++ Tasking_System/Tasking/Models/CoreUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Tasking.Models
+{
+    public class CoreUser
+    {
+        [Key]
+        [Display(Name = "Assiginee")]
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Phone { get; set; }
+        public string IdNumber { get; set; }
+
+    }
+}
